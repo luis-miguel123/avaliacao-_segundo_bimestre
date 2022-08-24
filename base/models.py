628665673432441b9edf6ddc468fa0a2cb2ac010
Luis_miguel123 <luis.m@escolar.ifrn.edu.br>
2022-08-24 17:53:06 -0300

    parte

--- base/models.py
+++ base/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+"""
+BEGIN;
+--
+-- Create model Question
+--
+CREATE TABLE "base_question" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "question_text" varchar(200) NOT NULL, "pub_date" datetime NOT NULL);
+--
+-- Create model Choice
+--
+CREATE TABLE "base_choice" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "choice_text" varchar(200) NOT NULL, "votes" integer NOT NULL, "question_id" bigint NOT NULL REFERENCES "base_question" ("id") DEFERRABLE INITIALLY DEFERRED);
+CREATE INDEX "base_choice_question_id_b7a693e8" ON "base_choice" ("question_id");
+COMMIT;
+"""
